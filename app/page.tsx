67d7af8f95fe3d56898e426b24c0f63df9332997
Facundo Venturi <venturifaku@gmail.com>
2025-09-23 23:39:27 -0300

    Inicio

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,303 @@
+"use client";
+
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+import { Boldonse, Birthstone, Poppins } from "next/font/google";
+import { useState } from "react";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+const boldonse = Boldonse({
+  weight: "400",
+  subsets: ["latin"],
+  // display: "swap",
+  variable: "--font-boldonse",
+});
+
+const birthstone = Birthstone({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-birthstone",
+});
+
+const poppinsBold = Poppins({
+  weight: "700",
+  subsets: ["latin"],
+  variable: "--font-poppins-bold",
+});
+
+const poppinsThin = Poppins({
+  weight: "200",
+  subsets: ["latin"],
+  variable: "--font-poppins-thin",
+});
+
+const SOCIAL_SIZE = 32;
+
+const MENU_ITEMS_BY_YEAR = {
+  1: [
+    "Fotografía",
+    "P.D.L.C",
+    "Edición de Sonido I",
+    "Prácticas de la Escritura",
+  ],
+  2: ["", "", "", ""],
+  3: ["", "", "", ""],
+};
+
+export default function Home() {
+  const [selectedYear, setSelectedYear] = useState(1);
+
+  return (
+    <main className="relative min-h-screen bg-cover bg-center text-white overflow-hidden">
+      <Image
+        className="-z-50 relative opacity-80"
+        src="/inicio/fondo-inicio.webp"
+        alt="Fondo inicio"
+        objectFit="cover"
+        fill
+        priority
+      />
+
+      {/* Click to show UI again */}
+      <div
+        onClick={() => {
+          const content = document.getElementById("content");
+          if (content) {
+            if (content.style.display === "none") {
+              content.style.display = "flex";
+              content.className += " animate-fade-in";
+              setTimeout(() => {
+                content.className = content.className.replace(
+                  " animate-fade-in",
+                  ""
+                );
+              }, 300);
+            }
+          }
+        }}
+        className="absolute inset-0 z-0"
+      ></div>
+
+      {/* Contenedor principal */}
+      <div
+        id="content"
+        className="relative z-10 flex flex-col justify-between min-h-screen"
+      >
+        {/* Header con redes */}
+        <div className="absolute top-0 right-0 p-8 flex justify-between items-center">
+          <div className="flex justify-end gap-8 text-2xl">
+            <a
+              href="https://www.instagram.com/ignaciosinfiltro/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Instagram ignaciosinfiltro"
+              className="hover:scale-125 transition"
+            >
+              <Image
+                src="/redes/instagram-png.png"
+                alt="Instagram logo"
+                width={SOCIAL_SIZE}
+                height={SOCIAL_SIZE}
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/ignaciocecilucero/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn ignaciocecilucero"
+              className="hover:scale-125 transition"
+            >
+              <Image
+                src="/redes/linkedin-png.png"
+                alt="LinkedIn logo"
+                width={SOCIAL_SIZE}
+                height={SOCIAL_SIZE}
+              />
+            </a>
+            <a
+              href="https://www.youtube.com/@ignaciosinfiltro"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="YouTube ignaciosinfiltro"
+              className="hover:scale-125 transition"
+            >
+              <Image
+                src="/redes/youtube-png.png"
+                alt="YouTube logo"
+                width={SOCIAL_SIZE * 1.5}
+                height={SOCIAL_SIZE}
+              />
+            </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        {/* Contenido principal */}
+        <div className=" flex flex-row h-screen p-[100px] justify-between items-center">
+          <div className="flex flex-col justify-between items-start h-full">
+            {/* Título*/}
+            <div className="flex flex-col">
+              <h1
+                className={
+                  `text-6xl my-shadow text-center leading-10 ` +
+                  boldonse.className
+                }
+              >
+                Frame <br />
+                <span className="text-3xl leading-12">x</span> <br />
+                Frame
+              </h1>
+              <p className={`mt-4 my-shadow text-3xl ` + birthstone.className}>
+                by Ignacio Ceci
+              </p>
+            </div>
+
+            {/* Descripción */}
+            <div className="max-w-lg flex flex-col justify-between">
+              <p className="text-[1.4rem] leading-[2.5rem] my-shadow ">
+                <span className={poppinsBold.className}>
+                  Expositor de trabajos
+                </span>{" "}
+                realizados <br /> en la carrera de{" "}
+                <span className={poppinsBold.className}>
+                  Producción Audiovisual
+                </span>
+                <br /> de la{" "}
+                <span className={poppinsBold.className}>
+                  Escuela de Comunicación ETER
+                </span>
+                .
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-between items-end h-full">
+            <div
+              className={
+                `flex flex-col items-end gap-6 my-shadow text-5xl text-end pt-16 overflow-hidden ` +
+                poppinsThin.className
+              }
+            >
+              {selectedYear !== 0 &&
+                MENU_ITEMS_BY_YEAR[selectedYear as 1 | 2 | 3].map((item) => (
+                  <span
+                    key={item}
+                    data-year={selectedYear}
+                    className="hover:scale-110 transition hover:-translate-x-6 cursor-pointer p-2 select-none animate-slide-in"
+                  >
+                    {item}
+                  </span>
+                ))}
+            </div>
+
+            <div
+              className={
+                `flex justify-between gap-16 text-white text-[2.75rem] select-none ` +
+                poppinsBold.className
+              }
+            >
+              <button
+                className={
+                  `px-2 my-shadow transition cursor-pointer` +
+                  (selectedYear === 1
+                    ? " scale-125 hover:scale-100 -translate-y-1/2"
+                    : " hover:scale-125")
+                }
+                onClick={() => {
+                  if (selectedYear !== 1) {
+                    setSelectedYear(1);
+                  } else {
+                    const year1list =
+                      document.querySelectorAll('[data-year="1"]');
+                    year1list.forEach((year1) => {
+                      if (year1) year1.className += " animate-slide-out";
+                    });
+                    setTimeout(() => setSelectedYear(0), 300);
+                  }
+                }}
+              >
+                1°
+              </button>
+              <button
+                disabled
+                className={
+                  `px-2 my-shadow transition text-neutral-400 blur-[1px] cursor-not-allowed` +
+                  (selectedYear === 2
+                    ? " scale-125 hover:scale-100 -translate-y-1/2"
+                    : " hover:scale-125")
+                }
+                onClick={() => {
+                  if (selectedYear !== 2) {
+                    setSelectedYear(2);
+                  } else {
+                    const year2list =
+                      document.querySelectorAll('[data-year="2"]');
+                    year2list.forEach((year2) => {
+                      if (year2) year2.className += " animate-slide-out";
+                    });
+                    setTimeout(() => setSelectedYear(0), 300);
+                  }
+                }}
+              >
+                2°
+              </button>
+              <button
+                disabled
+                className={
+                  `px-2 my-shadow transition text-neutral-400 blur-[1px] cursor-not-allowed` +
+                  (selectedYear === 3
+                    ? " scale-125 hover:scale-100 -translate-y-1/2"
+                    : " hover:scale-125")
+                }
+                onClick={() => {
+                  if (selectedYear !== 3) {
+                    setSelectedYear(3);
+                  } else {
+                    const year3list =
+                      document.querySelectorAll('[data-year="3"]');
+                    year3list.forEach((year3) => {
+                      if (year3) year3.className += " animate-slide-out";
+                    });
+                    setTimeout(() => setSelectedYear(0), 300);
+                  }
+                }}
+              >
+                3°
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Boton flotante para ocultar UI */}
+        <button
+          onClick={() => {
+            const content = document.getElementById("content");
+            if (content) {
+              if (content.style.display === "none") {
+                content.style.display = "flex";
+              } else {
+                content.className += " animate-fade-out";
+                setTimeout(() => {
+                  content.style.display = "none";
+                  content.className = content.className.replace(
+                    " animate-fade-out",
+                    ""
+                  );
+                }, 300);
+              }
+            }
+          }}
+          className="absolute bottom-4 left-1/2 p-2 bg-black opacity-30 hover:opacity-70 rounded-full cursor-pointer hover:bg-opacity-75 transition z-20"
         >
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/icons/eye-slash-regular-full.svg"
+            className="invert"
+            alt="Ocultar UI"
+            width={24}
+            height={24}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </button>
+      </div>
+    </main>
   );
 }
+
+// text-neutral-400 blur-[1px] cursor-not-allowed
